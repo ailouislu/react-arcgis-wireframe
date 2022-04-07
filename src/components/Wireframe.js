b1e01068404af131efc7f1eae841a35d1c41bc37
LouisAugustine <nzlouis.com@gmail.com>
2022-04-07 16:12:10 +1200

    Added Expand

--- src/components/Wireframe.js
+++ src/components/Wireframe.js
@@ -1,6 +1,13 @@
 import React, {useEffect} from 'react';
 import Map from "@arcgis/core/WebMap";
 import MapView from "@arcgis/core/views/MapView"
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer"
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer"
+import Expand from "@arcgis/core/widgets/Expand"
+import Legend from "@arcgis/core/widgets/Legend"
+import LayerList from "@arcgis/core/widgets/LayerList"
+import BasemapGallery from "@arcgis/core/widgets/BasemapGallery"
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../styles/Wireframe.css";
 
@@ -10,6 +17,8 @@ export default function Wireframe() {
     setupWebMap();
   }, []);
 
+  
+
   const setupWebMap = () => {
 
     // const webMap = new WebMap({
@@ -18,11 +27,87 @@ export default function Wireframe() {
     //   }
     // });
 
+    const defaultSym = {
+      type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+      color: [0, 0, 0, 0],
+      outline: {
+        // autocasts as new SimpleLineSymbol()
+        color: "#71de6e",
+        width: 1
+      }
+    };
+
+    const renderer = {
+      type: "simple", // autocasts as new SimpleRenderer()
+      symbol: defaultSym,
+      visualVariables: [
+        {
+          type: "size",
+          field: "NITROGEN",
+          normalizationField: "NITROGEN",
+          legendOptions: {
+            title: "% population in poverty by county"
+          },
+          stops: [
+            {
+              value: 0.15,
+              size: 4,
+              label: "<15%"
+            },
+            {
+              value: 0.25,
+              size: 12,
+              label: "25%"
+            },
+            {
+              value: 0.35,
+              size: 20,
+              color: [0, 0, 0, 0],
+              label: ">35%"
+            }
+          ]
+        }
+      ]
+    };
+
+    const povLayer = new FeatureLayer({
+      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/counties_politics_poverty/FeatureServer/0",
+      renderer: renderer,
+      title: "State of NZ water quality",
+      popupTemplate: {
+        // autocasts as new PopupTemplate()
+        title: "{NAME}",
+        content:
+          "{NAME} State of NZ water quality",
+        fieldInfos: [
+          {
+            fieldName: "NITROGEN",
+            format: {
+              digitSeparator: true,
+              places: 0
+            }
+          },
+          {
+            fieldName: "PHOSPHORUS",
+            format: {
+              digitSeparator: true,
+              places: 0
+            }
+          }
+        ]
+      }
+    });
+
+
+
     const map = new Map({
       basemap: "gray-vector",
+      layers: [povLayer],
+      // basemap: "arcgis-topographic", //Basemap layer service
       portalItem: {
         id: 'c3337bfc8f964688856b36a4651b66cf'
       }
+      
     });
 
 
@@ -36,47 +121,47 @@ export default function Wireframe() {
       }
     });
 
-    
+      // add legend, layerlist and basemapGallery widgets
+      view.ui.add(
+        [
+          new Expand({
+            content: new Legend({
+              view: view
+            }),
+            view: view,
+            group: "top-left"
+          }),
+          new Expand({
+            content: new LayerList({ view: view }),
+            view: view,
+            group: "top-left"
+          }),
+          new Expand({
+            content: new BasemapGallery({
+              view: view
+            }),
+            view: view,
+            expandIconClass: "esri-icon-basemap",
+            group: "top-left"
+          })
+        ],
+        "top-left"
+      );
 
-    // // add legend, layerlist and basemapGallery widgets
-    // view.ui.add(
-    //   [
-    //     new Expand({
-    //       content: new Legend({
-    //         view: view
-    //       }),
-    //       view: view,
-    //       group: "top-left"
-    //     }),
-    //     new Expand({
-    //       content: new LayerList({ view: view }),
-    //       view: view,
-    //       group: "top-left"
-    //     }),
-    //     new Expand({
-    //       content: new BasemapGallery({
-    //         view: view
-    //       }),
-    //       view: view,
-    //       expandIconClass: "esri-icon-basemap",
-    //       group: "top-left"
-    //     })
-    //   ],
-    //   "top-left"
-    // );
 
     // view.popup.autoOpenEnabled = false;  // Disable the default popup behavior
-    view.on("click", function(event) { // Listen for the click event
-      view.hitTest(event).then(function (hitTestResults){ // Search for features where the user clicked
-        if(hitTestResults.results) {
-          view.popup.open({ // open a popup to show some of the results
-            location: event.mapPoint,
-            title: "Hit Test Results",
-            content: hitTestResults.results
-          });
-        }
-      })
-    });
+    // view.on("click", function(event) { // Listen for the click event
+    //   view.hitTest(event).then(function (hitTestResults){ // Search for features where the user clicked
+    //     if(hitTestResults.results) {
+    //       view.popup.open({ // open a popup to show some of the results
+    //         location: event.fieldName,
+    //         title: "Hit Test Results",
+    //         content: hitTestResults.results
+    //       });
+    //     }
+    //   })
+    // });
+
 
     // Adds widget below other elements in the top left corner of the view
     view.ui.add("stateOfWaterQuality", {
