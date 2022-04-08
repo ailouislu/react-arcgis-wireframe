6110ec54cf282cf15938d94e4174cf54704f19a5
LouisAugustine <nzlouis.com@gmail.com>
2022-04-09 01:04:09 +1200

    Add test methods

--- src/__test__/Wireframe.test.js
+++ src/__test__/Wireframe.test.js
@@ -4,6 +4,20 @@ import Wireframe from '../components/Wireframe';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
+import { cleanup} from '@testing-library/react'
+import { getAverageNitrogen, getAveragePhosphorus, getNationalStandard } from "../services/fakeWaterQualityService";
+
+afterEach(cleanup);
+
+test('should return correct data for the water quality', () => {
+    const averageNitrogen =  getAverageNitrogen();
+    const averagePhosphorus =  getAveragePhosphorus();
+    const nationalStandard =  getNationalStandard();
+    expect(averageNitrogen).toBe("137.1");
+    expect(averagePhosphorus).toBe("33.6");
+    expect(nationalStandard).toBe("48.9");
+})
+
 Enzyme.configure({ adapter: new Adapter() });
 
 const props = {
