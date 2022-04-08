ba4e177835a88ce3cb33b72a33165585836b7ece
LouisAugustine <nzlouis.com@gmail.com>
2022-04-08 22:07:55 +1200

    Add two points

--- src/components/Wireframe.js
+++ src/components/Wireframe.js
@@ -2,11 +2,17 @@ import React, {useEffect} from 'react';
 import Map from "@arcgis/core/WebMap";
 import MapView from "@arcgis/core/views/MapView"
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer"
-import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer"
+import Point from "@arcgis/core/geometry/Point"
+import Graphic from "@arcgis/core/Graphic"
 import Expand from "@arcgis/core/widgets/Expand"
 import Legend from "@arcgis/core/widgets/Legend"
 import LayerList from "@arcgis/core/widgets/LayerList"
 import BasemapGallery from "@arcgis/core/widgets/BasemapGallery"
+import CoordinateConversion from "@arcgis/core/widgets/CoordinateConversion"
+import PopupTemplate from "@arcgis/core/PopupTemplate";
+import * as watchUtils from "@arcgis/core/core/watchUtils";
+import * as univariateRendererCreator from "@arcgis/core/smartMapping/renderers/univariateColorSize"
+import * as sizeRendererCreator from "@arcgis/core/smartMapping/renderers/size"
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../styles/Wireframe.css";
@@ -27,82 +33,10 @@ export default function Wireframe() {
     //   }
     // });
 
-    const defaultSym = {
-      type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-      color: [0, 0, 0, 0],
-      outline: {
-        // autocasts as new SimpleLineSymbol()
-        color: "#71de6e",
-        width: 1
-      }
-    };
-
-    const renderer = {
-      type: "simple", // autocasts as new SimpleRenderer()
-      symbol: defaultSym,
-      visualVariables: [
-        {
-          type: "size",
-          field: "NITROGEN",
-          normalizationField: "NITROGEN",
-          legendOptions: {
-            title: "% population in poverty by county"
-          },
-          stops: [
-            {
-              value: 0.15,
-              size: 4,
-              label: "<15%"
-            },
-            {
-              value: 0.25,
-              size: 12,
-              label: "25%"
-            },
-            {
-              value: 0.35,
-              size: 20,
-              color: [0, 0, 0, 0],
-              label: ">35%"
-            }
-          ]
-        }
-      ]
-    };
-
-    const povLayer = new FeatureLayer({
-      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/counties_politics_poverty/FeatureServer/0",
-      renderer: renderer,
-      title: "State of NZ water quality",
-      popupTemplate: {
-        // autocasts as new PopupTemplate()
-        title: "{NAME}",
-        content:
-          "{NAME} State of NZ water quality",
-        fieldInfos: [
-          {
-            fieldName: "NITROGEN",
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          },
-          {
-            fieldName: "PHOSPHORUS",
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          }
-        ]
-      }
-    });
-
-
 
     const map = new Map({
       basemap: "gray-vector",
-      layers: [povLayer],
+      // layers: [layer]
       // basemap: "arcgis-topographic", //Basemap layer service
       portalItem: {
         id: 'c3337bfc8f964688856b36a4651b66cf'
@@ -110,16 +44,16 @@ export default function Wireframe() {
       
     });
 
-
     const view = new MapView({
       container: "mapView",
       map: map,
       center: [176.7598, -41.6899],
       zoom: 4,
       highlightOptions: {
-        color: "orange"
+        // color: "orange"
       }
     });
+
 
       // add legend, layerlist and basemapGallery widgets
       view.ui.add(
@@ -148,22 +82,67 @@ export default function Wireframe() {
         "top-left"
       );
 
+      const ccWidget = new CoordinateConversion({
+        view: view
+      });
 
-    // view.popup.autoOpenEnabled = false;  // Disable the default popup behavior
-    // view.on("click", function(event) { // Listen for the click event
-    //   view.hitTest(event).then(function (hitTestResults){ // Search for features where the user clicked
-    //     if(hitTestResults.results) {
-    //       view.popup.open({ // open a popup to show some of the results
-    //         location: event.fieldName,
-    //         title: "Hit Test Results",
-    //         content: hitTestResults.results
-    //       });
-    //     }
-    //   })
-    // });
+      view.ui.add(ccWidget, "bottom-left");
 
+      // Create a symbol for drawing the point
+      const markerSymbol = {
+        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+        color: [226, 119, 40],
+        outline: {
+          // autocasts as new SimpleLineSymbol()
+          color: [255, 255, 255],
+          width: 2
+        }
+      };
 
-    // Adds widget below other elements in the top left corner of the view
+      const point1 = {
+        type: "point", // autocasts as new Point()
+        longitude: 175.1557,
+        latitude: -40.6978
+      };
+
+      // Create a graphic and add the geometry and symbol to it
+      const pointGraphic1 = new Graphic({
+        geometry: point1,
+        symbol: markerSymbol
+      });
+
+      const point2 = {
+        type: "point", // autocasts as new Point()
+        longitude: 177.7598,
+        latitude: -42.6899
+      };
+
+      
+      const pointGraphic2 = new Graphic({
+        geometry: point2,
+        symbol: markerSymbol
+      });
+      
+
+      view.graphics.addMany([pointGraphic1, pointGraphic2]);
+
+    view.popup.autoOpenEnabled = false;  // Disable the default popup behavior
+    view.on("click", function(event) { // Listen for the click event
+      view.hitTest(event.screenPoint).then(function (response){ // Search for features where the user clicked
+        var graphic = response.results[0].graphic.attributes.OBJECTID;
+        if(graphic) {
+          view.popup.open({ // open a popup to show some of the results
+            location: event.mapPoint,
+            title: "Site:"+ graphic,
+            content: event.mapPoint.x + ",  "+ event.mapPoint.y
+          });
+        }else{
+          view.popup.close();
+        }
+      })
+    });
+
+    // Adds widget below other elements in the top right corner of the view
     view.ui.add("stateOfWaterQuality", {
       position: "top-right"
     });
@@ -175,6 +154,7 @@ export default function Wireframe() {
     <div className="app-container">
     {/* Load esri API and the map */}
       <div id="mapView" className='map-view'/>
+
       <div id="stateOfWaterQuality" className="state" >
         <div className="state-title">State of NZ water quality</div>
         <div className="state-circle circle1">
